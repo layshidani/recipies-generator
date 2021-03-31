77f7055fafd94a42b16690bfdcb7896ab5d48ebd
Lays Marie Hidani <lays.hidani@gmail.com>
2021-03-30 22:22:23 -0300

    feat: add cardListComponent :sparkles:

---
+++ src/app/card-list/card-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+import * as data from '../../../data/afrodite.json';
+
+
+@Component({
+  selector: 'app-card-list',
+  templateUrl: './card-list.component.html',
+  styleUrls: ['./card-list.component.css']
+})
+export class CardListComponent implements OnInit {
+  public content = data['default'].slice(0, 10);
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+    console.warn('content', this.content);
+
+  }
+
+}
